64f97c55cbe441ed826a758b3d4dec0434fbd907
tiago <tiago001pessoa@gmail.com>
2023-10-22 16:21:12 -0300

    first commit

---
+++ src/main/java/me/dio/desafioapi/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiApplication.class, args);
+	}
+
+}
